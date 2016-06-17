d443f3129b0c4fe94be4cd8d2f4eea2ed2c00be2
Kai Curry <kai@cape.io>
2016-06-16 22:04:28 -0500

    Allow removal of favs from list

--- src/components/Favs.js
+++ src/components/Favs.js
@@ -1,38 +1,54 @@
 import React, { PropTypes } from 'react'
 import map from 'lodash/map'
-import Menu from './Menu'
+import partial from 'lodash/partial'
 
-function FavsList({ favorites }) {
+import Menu from './Menu'
+import FavButton from './FavButton'
+
+function FavItem({ item, onClick }) {
+  return (
+    <li>
+      <FavButton favorited item={item} onClick={onClick} />
+      <img src={item.img} alt={item.id} title={item.id} />
+    </li>
+  )
+}
+FavItem.propTypes = {
+  item: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired,
+}
+
+function FavsList({ endFavorite, favorites }) {
   return (
     <div>
       <h2>items</h2>
       <ul>
-        {map(favorites, ({ id }) => (
-          <li key={id}>
-            <h3>{id}</h3>
-          </li>
+        {map(favorites, (fav) => (
+          <FavItem key={fav.id} onClick={partial(endFavorite, fav)} item={fav.item} />
         ))}
       </ul>
     </div>
   )
 }
 FavsList.propTypes = {
+  endFavorite: PropTypes.func.isRequired,
   favorites: PropTypes.array.isRequired,
 }
 
-function Favs({ favorites, menu }) {
+function Favs({ endFavorite, favorites, menu }) {
   const hasFavorites = favorites && favorites.length > 0
   return (
     <div id="favorites">
       <Menu links={menu} />
       <h1>Favorites</h1>
-      {hasFavorites && <FavsList favorites={favorites} />}
+      {hasFavorites && <FavsList endFavorite={endFavorite} favorites={favorites} />}
       {!hasFavorites && <p>No favorites...</p>}
     </div>
   )
 }
 
 Favs.propTypes = {
+  endFavorite: PropTypes.func.isRequired,
   favorites: PropTypes.array.isRequired,
   menu: PropTypes.array.isRequired,
 }

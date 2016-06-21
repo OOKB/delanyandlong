c4b593287b77df79b71814ce0e29a8605f6e5878
Kristian Bjornard <kb@ookb.co>
2016-06-21 14:31:27 -0400

    footer content starting to be there

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,11 @@ import React, { PropTypes } from 'react'
 
 function Footer() {
   return (
-    <footer className="text-center mb4 mt1 p1">
-      <p className="bt1 pt2">
-        More To Come.
+    <footer className="text-center mb4 mt1 p1 clear">
+      <p className="bt1 pt2 uppercase">
+        &copy; Delany &amp; Long LTD.
       </p>
+      <p className="small">site by OOKB/powered by CAPE</p>
     </footer>
   )
 }

aa312edf0ea9374428a9954c96760662e4e1d0f5
Kristian Bjornard <kb@ookb.co>
2016-07-01 12:19:45 -0400

    adding styles to convert to JS as a comment...

--- src/components/Pricelist/styles.js
+++ src/components/Pricelist/styles.js
@@ -15,3 +15,40 @@ export const cellStyles = {
     paddingTop: '0.5em',
   },
 }
+
+/*
+#container-pricelist table tr th,
+#container-pricelist table tr td {
+  line-height: 1.5em;
+  vertical-align: middle;
+  border-collapse: collapse;
+  border: 0;
+  font-weight: normal;
+  padding: 1px .66em;
+}
+
+#container-pricelist tbody tr:hover {
+  background-color: rgba(132,133,94,.1);
+}
+tr:nth-child(odd) {
+  background-color: rgba(200,200,190,.1);
+}
+
+#container-pricelist table .pattern td {
+  border-top: 1px solid rgba(200,200,190,.25);
+}
+
+#container-pricelist td img {
+  vertical-align: middle;
+}
+
+td.price {
+  width: 100px;
+}
+td.img {
+  width: 50px;
+}
+td.contents {
+  width: 33%;
+}
+*/

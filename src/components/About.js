9cf597f714e59235f596ec50ce7cab5bc8129a6e
Kristian Bjornard <kb@ookb.co>
2016-07-15 13:33:09 -0400

    fix issue #25

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,6 @@ function About({ aboutText, serviceText, menu }) {
       <Header links={menu} />
       <main className="clear pt4 container">
         <div className="group">
-          <img src="https://images.unsplash.com/photo-1459128806329-1b61d19a0f93?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=800&h=400&fit=crop&s=d7eb9a7a335cbd55db5ecd2db7bf1afd" alt="About Page header..." className="eight columns offset-by-two" />
           <div className="about six columns offset-by-three">
             <div dangerouslySetInnerHTML={{ __html: aboutText }} />
             <p className="small mt3 bt1 pt2">{serviceText}</p>

98b35d64347f33a526f6a75fa842e5388fb638eb
Kristian Bjornard <kb@ookb.co>
2016-07-26 23:52:23 -0400

    trying out some more in component styles

--- src/components/LinkOrNot.js
+++ src/components/LinkOrNot.js
@@ -1,8 +1,19 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'redux-history-sync'
 
+const styles = {
+  active: {
+    display: 'block',
+    height: '100%',
+    width: '100%',
+    overflow: 'hidden',
+    boxShadow: 'inset 0 0 10px rgba(66,67,47,.5)',
+    border: '2px solid rgba(66,67,47,.5)',
+  },
+}
+
 function LinkOrNot({ children, color, parent }) {
-  if (parent.id === color.id) return <span style={{ opacity: 0.5 }}>{children}</span>
+  if (parent.id === color.id) return <span style={styles.active}>{children}</span>
   return <Link href={color.link} title={color.id}>{children}</Link>
 }
 

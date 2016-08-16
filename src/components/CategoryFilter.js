076e9f2abb1378cd87fc2d88b06913491c8d718a
Kristian Bjornard <kb@ookb.co>
2016-08-16 14:22:49 -0400

    some filter header clieanup

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -24,14 +24,14 @@ function getButtonStyle(activeCategory, value) {
 function CategoryFilter({ activeCategory, formEvent, options }) {
   return (
     <div className="collection-menu group mb0">
-      <h2 className="twelve columns mb0 uppercase">Filters</h2>
-      <div className="select-type">
-        <p>Select type:</p>
-        <div className="button-group">
+      {/* <h2 className="twelve columns mb0 uppercase">Filters</h2> */}
+      <div className="twelve columns mb0 select-type">
+        <div className="button-group flex-center text-center">
+          <p className="uppercase m0">Filter By Type:</p>
           {map(options, ({ value, label }) => {
             const style = getButtonStyle(activeCategory, value)
             return (
-              <button style={style} onClick={formEvent.onChange} value={value} key={value}>
+              <button className="flex-item" style={style} onClick={formEvent.onChange} value={value} key={value}>
                 {label}
               </button>
             )

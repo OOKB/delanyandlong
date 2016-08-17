06627917fb2478ef7e1a52dd7c5e22418a69d6e3
Kai Curry <kai@cape.io>
2016-08-17 10:17:55 -0500

    messing around with projects

---
+++ src/redux/dispatch/db.js
@@ -0,0 +1,47 @@
+export function nextId() {
+  return Math.random().toString(36).substr(6)
+  .substring(1, 9)
+}
+export function insertFields(data) {
+  return {
+    ...data,
+    dateCreated: new Date(),
+    id: nextId(),
+  }
+}
+
+export function createCollectionItem(item, position = 100) {
+  return {
+    actionStatus: 'created',
+    item,
+    position,
+    startTime: new Date(),
+    type: 'ListItem',
+  }
+}
+// Adding an item to a list requires a new triple.
+export function createCollectionItemTriple(list, item, position) {
+  // Create the ListItem.
+  const object = createCollectionItem(item, position)
+  // The item is attached to the list by adding an itemListElement predicate triple.
+  return { subject: list, predicate: 'itemListElement', object }
+}
+export function createCollectionList(creator) {
+  const mainEntity = { id: 'pBlf', type: 'DataFeed' }
+  return {
+    additionalType: 'ProjectDelanyLong',
+    creator, // User that created the thing.
+    itemListOrder: 'Ascending',
+    mainEntity, // List of what.
+    type: 'CollectionList',
+  }
+}
+
+export function favoriteItem(item, listId) {
+  return (dispatch) => {
+    // const state = getState()
+    const list = { type: 'CollectionList', id: listId }
+    const listItem = createCollectionItemTriple(list, item)
+    dispatch(createTriple(listItem))
+  }
+}
--- src/redux/dispatch/favs.js
+++ src/redux/dispatch/favs.js
@@ -1,37 +1,26 @@
-import { entityPut, entityUpdate, triplePutAll } from 'redux-graph'
+import { entityPutAll, entityUpdate, triplePutAll } from 'redux-graph'
 import { getUser } from '../select'
 import { getItemDetail } from '../select/item'
+import { createBookmarkAction, createItemList } from './db'
 
-export function newKey() {
-  return Math.random().toString(36).substr(6)
-  .substring(1, 9)
-}
-export function favAction() {
-  const now = new Date()
-  return {
-    actionStatus: 'created',
-    dateCreated: now,
-    description: 'An anonymous user of delanyandlong.com likes an item.',
-    id: newKey(),
-    position: 100,
-    startTime: now,
-    type: 'LikeAction',
-  }
-}
-export function favTriples(subject, agent, object) {
+export function favTriples(action, agent, object, list) {
   return [
-    { subject, predicate: 'agent', object: agent },
-    { subject, predicate: 'object', object },
+    { subject: action, predicate: 'agent', object: agent }, // User that likes the thing.
+    { subject: action, predicate: 'object', object }, // Thing that the user likes.
+    { subject: action, predicate: 'result', list }, // The project this like attached to.
+    { subject: list, predicate: 'creator', agent },
   ]
 }
+
 export function favoriteItem(item) {
   return (dispatch, getState) => {
     const state = getState()
     const agent = getUser(state)
-    const subject = favAction()
+    const action = createBookmarkAction()
+    const list = createItemList()
     const object = item || getItemDetail(state)
-    const triples = favTriples(subject, agent, object)
-    dispatch(entityPut(subject))
+    const triples = favTriples(action, agent, object, list)
+    dispatch(entityPutAll([ action, list ]))
     dispatch(triplePutAll(triples))
   }
 }

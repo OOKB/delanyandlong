23969c57252868786118d6e82fd3d39cebdf5934
Kristian Bjornard <kb@ookb.co>
2016-08-18 22:35:56 -0400

    trying to clean up detail page a bit more

--- src/components/Detail/Related.js
+++ src/components/Detail/Related.js
@@ -4,8 +4,8 @@ import map from 'lodash/map'
 import Color from './RelatedColor'
 
 function getStyle(isOpen) {
-  if (isOpen) return { height: 'calc(100% - 25px - 1.3em - 1.3em)' }
-  return { height: 'auto' }
+  if (isOpen) return { height: 'calc(100% - 25px - 1.3em - 1.3em)', boxShadow: '0 .25em .75em 0 rgba(65,65,45,.75)' }
+  return { height: 'calc(3rem + 4px)', boxShadow: '0 0 0 0 rgba(65,65,45,0)' }
 }
 
 function Related({ close, colors, isOpen, open, parent }) {

860d61ee5b24a1c78d4361842574a633709910ce
Kai Curry <kai@cape.io>
2016-08-23 11:57:39 -0500

    cleanup fav

--- src/redux/dispatch/db.js
+++ src/redux/dispatch/db.js
@@ -1,13 +1,1 @@
 import { createTriple } from 'redux-graph'
-
-
-
-
-export function favoriteItem(item, listId) {
-  return (dispatch) => {
-    // const state = getState()
-    const list = { type: 'CollectionList', id: listId }
-    const listItem = createCollectionItemTriple(list, item)
-    dispatch(createTriple(listItem))
-  }
-}

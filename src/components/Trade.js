25aa84dd1057257a44bfce9ee07b496db25ced91
Kristian Bjornard <kb@ookb.co>
2016-08-23 12:35:50 -0400

    temporarily working out some trade stuff visually

--- src/components/Trade.js
+++ src/components/Trade.js
@@ -9,7 +9,30 @@ function Trade({ menu, route }) {
   return (
     <div id="trade">
       <Header activeId={route.id} links={menu} />
-      <p>Trade Login</p>
+      <main className="container">
+        <div className="login-wrapper">
+          <div className="input-group">
+            <label>
+              D&L Account Number
+            </label>
+            <input />
+          </div>
+          <div className="input-group">
+            <label>
+              Zip Code
+            </label>
+            <input />
+          </div>
+          <button className="btn-outline" type="submit">Log In</button>
+        </div>
+        <div className="help-text small mt4 bt1">
+          <ul className="list-reset">
+            <li><p><strong>Have an existing account with us but don't know your login information?</strong><br />Use your full account number as your user name and ZIP code as your password.</p></li>
+            <li><p><strong>If you are a trades person and do not have an existing account...</strong><br />Please contact us at <a href="customerservice@delanyandlong.com">customerservice@delanyandlong.com</a>.</p></li>
+            <li><p>Accounts are available to trades people only (designers, re-sellers and industry members) and are not meant for regular customers.</p></li>
+          </ul>
+        </div>
+      </main>
       <Footer />
     </div>
   )

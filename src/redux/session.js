72bc7b22337d2180fd4b5c133f7014724cad5e2a
Kai Curry <kai@cape.io>
2016-09-08 18:07:33 -0500

    improve session reducer

--- src/redux/session.js
+++ src/redux/session.js
@@ -1,7 +1,22 @@
+import immutable from 'seamless-immutable'
+import isFunction from 'lodash/isFunction'
+import isObject from 'lodash/isObject'
+
 export function selectYear(state) {
   return state.session.currentYear
 }
-
-export default function reducer(state = {}) {
-  return state
+export const defaultState = immutable({
+  currentYear: new Date().getFullYear(),
+})
+export const UPDATE_PROP = 'session/UPDATE_PROP'
+export function updateProp(state, { property, value }) {
+  return property && value && state.set(property, value) || state
+}
+const reducers = {
+  [UPDATE_PROP]: updateProp,
+}
+export default function reducer(state = defaultState, action) {
+  if (action.error || !action.type || !isFunction(reducers[action.type])) return state
+  if (!isObject(action.payload)) return state
+  return reducers[action.type](state, action.payload)
 }

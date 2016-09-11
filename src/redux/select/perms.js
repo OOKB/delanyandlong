765a24ce61a44fda9db6f0264fa45ac50871d2d6
Kai Curry <kai@cape.io>
2016-09-11 13:11:07 -0500

    s/hasFavorites/userHasFavorites

--- src/redux/select/perms.js
+++ src/redux/select/perms.js
@@ -1,9 +1,9 @@
 import { createStructuredSelector } from 'reselect'
 import { isAnonymous, isAuthenticated } from '../auth/select'
-import { hasFavorites } from '../project/select'
+import { userHasFavorites } from '../project/select'
 
 const permissions = createStructuredSelector({
-  hasFavorites,
+  hasFavorites: userHasFavorites,
   isAnonymous,
   isAuthenticated,
 })

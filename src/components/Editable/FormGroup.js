11e047de38273ccb4259737dcd958d2ba9ec2cfc
Kai Curry <kai@cape.io>
2016-09-13 10:38:14 -0500

    FormGroup

---
+++ src/components/Editable/FormGroup.js
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react'
+
+import classNames from 'classnames'
+
+// Editable formGroup.
+
+function FormGroup({ label, status, id, required, children, editable, className }) {
+  const cssClasses = {
+    editable,
+    'form-group': true,
+    'has-error': (status === 'error'),
+    'has-success': (status === 'success'),
+    'has-warning': (status === 'warning'),
+    'has-feedback': status,
+  }
+
+  return (
+    <div className={classNames(cssClasses, className)} id={`${id}-group`}>
+      {label &&
+        <label className="control-label col-md-3" htmlFor={id}>
+          {label}
+          {required ? '*' : false}
+        </label>
+      }
+      {children}
+    </div>
+  )
+}
+
+FormGroup.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  editable: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  required: PropTypes.bool,
+  status: PropTypes.string,
+}
+
+FormGroup.defaultProps = {
+}
+
+export default FormGroup

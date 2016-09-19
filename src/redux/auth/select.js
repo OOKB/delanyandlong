1cf41f46f1902db5790b1ff0783b98a0fc1dfd2e
Kai Curry <kai@cape.io>
2016-09-19 18:27:42 -0500

    rm auth

--- src/redux/auth/select.js
+++
@@ -1,9 +0,0 @@
-import negate from 'lodash/negate'
-
-import { select } from '../utils'
-
-export const selectAuth = select('auth')
-export const selectUser = select('user', selectAuth)
-export const isAuthenticated = select('authenticated', selectAuth)
-export const isAnonymous = negate(isAuthenticated)
-export const selectUid = select('id', selectUser)

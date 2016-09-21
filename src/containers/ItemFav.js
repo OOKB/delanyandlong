e0e843c8bffddfda049b2d475f4f34260134285b
Kai Curry <kai@cape.io>
2016-09-20 20:56:25 -0500

    fix ItemFav and Trade

--- src/containers/ItemFav.js
+++ src/containers/ItemFav.js
@@ -1,17 +1,15 @@
 import { connect } from 'react-redux'
 import partial from 'lodash/partial'
 
-import { mapDispatchToProps } from '../redux/utils'
+import { mapDispatchToProps } from 'cape-redux'
 import { mapStateToProps } from '../redux/project/select'
 import { editItemCollections } from '../redux/project/actions'
+import Component from '../components/Fav'
 
 function getActions({ item }) {
   return {
     editItemCollections: partial(editItemCollections, item),
   }
 }
-
-
-import Component from '../components/Fav'
 
 export default connect(mapStateToProps, mapDispatchToProps(getActions))(Component)

1d788f985663b0109a13be163d42a73058a164ff
Kristian Bjornard <kb@ookb.co>
2016-10-04 12:15:18 -0400

    figuring out some component inline styling

--- src/components/Editable/input/InputClear.js
+++ src/components/Editable/input/InputClear.js
@@ -6,13 +6,29 @@ const styles = {
   base: {
     position: 'absolute',
     top: 0,
-    // bottom: 0,
+    bottom: 0,
     right: 4,
     border: 'none',
     background: 'transparent',
     color: 'currentColor',
+    height: 'auto',
+    width: '1rem',
+    textAlign: 'right',
+    margin: 0,
+    padding: 0,
     ':hover': {
+      background: 'transparent',
+      color: 'rgba(189,178,119,0.5)',
+    },
+    ':focus': {
+      background: 'transparent',
+      color: 'rgba(189,178,119,0.75)',
+      boxShadow: 'none',
+    },
+    ':active': {
+      background: 'rgba(189,178,119,0.25)',
       color: 'rgba(103,099,049,1)',
+      boxShadow: 'none',
     },
   },
 }

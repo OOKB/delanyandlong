bb9dec3c103562b576c92a41e486dcac0a017939
Kai Curry <kai@cape.io>
2016-10-07 09:48:57 -0400

    use new images!

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -15,7 +15,7 @@ function Detail({ detailClose, item, route }) {
     <div id="detailWrapper" className={classnames('absolute', { editing })}>
       <DetailHeader />
       <Info item={item} />
-      <img className="fixed" src={item.img} alt={item.id} style={{ zIndex: -1 }} />
+      <img className="fixed" src={`${item.img}?w=1500`} alt={item.id} style={{ zIndex: -1 }} />
       {editing &&
         <Images subject={item} style={{ marginTop: 100 }} />
       }

c0ff157e4409998b4a2ecf55f622bf7606dd06bd
Kai Curry <kai@cape.io>
2016-10-07 22:36:54 -0400

    change how to select items

--- src/redux/select/items.js
+++ src/redux/select/items.js
@@ -1,23 +1,12 @@
 import { createSelector } from 'reselect'
-import { entitySelector } from 'redux-graph'
-import compact from 'lodash/compact'
-import curry from 'lodash/curry'
-import every from 'lodash/every'
-import flatten from 'lodash/flatten'
-import filter from 'lodash/filter'
-import includes from 'lodash/includes'
-import map from 'lodash/map'
-import mapValues from 'lodash/mapValues'
-import orderBy from 'lodash/orderBy'
-import pickBy from 'lodash/pickBy'
-import uniq from 'lodash/uniq'
+import { entityTypeSelector } from 'redux-graph'
+import {
+  compact, curry, every, flatten, filter, includes, map, mapValues, orderBy, uniq,
+} from 'lodash'
 
 import { getFilter, getFilterText } from './'
 import { activeCategorySelector, categoryCodeIndex } from './category'
 
-export function isValidItem(entity) {
-  return entity.id.startsWith('DL')
-}
 export const activeColor = getFilter('color')
 // const CDN = 'https://3f363c8bf5767a720417-fdf7aa33c10c7fb6e1c8c4e342fa358c.ssl.cf5.rackcdn.com'
 const CDN = 'https://delanyandlong.imgix.net'
@@ -37,10 +26,7 @@ export function itemFill(item, catCodeIndex) {
     searchable: (color + contents + name + id).toLowerCase(),
   }
 }
-export const itemsRaw = createSelector(
-  entitySelector,
-  entity => pickBy(entity, isValidItem)
-)
+export const itemsRaw = entityTypeSelector('OrderTrackItem')
 export const itemsFilled = createSelector(
   itemsRaw,
   categoryCodeIndex,
@@ -81,7 +67,7 @@ export const textSearchSelector = createSelector(
 )
 export const patternColorSelector = createSelector(
   textSearchSelector,
-  items => {
+  (items) => {
     let currentPattern = null
     return map(items, item => {
       const isPattern = currentPattern !== item.patternNumber

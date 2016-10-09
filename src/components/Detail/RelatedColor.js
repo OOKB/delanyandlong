e11cdf30ae14f4c4996ce9c7c7fda91909de6c64
Kristian Bjornard <kb@ookb.co>
2016-10-08 22:18:59 -0400

    cleaning up styles on detail and pricelist

--- src/components/Detail/RelatedColor.js
+++ src/components/Detail/RelatedColor.js
@@ -6,7 +6,7 @@ function RelatedColor({ color, parent }) {
   return (
     <li>
       <LinkOrNot color={color} parent={parent} >
-        <img src={color.img.concat('?w=100&h=100&crop=focalpoint&fit=crop&fp-x=.5&fp-y=.5&fp-z=1.5')} alt={color.id} />
+        <img src={color.img.concat('?w=150&h=150&crop=focalpoint&fit=crop&fp-x=.5&fp-y=.5&fp-z=1.5')} alt={color.id} />
       </LinkOrNot>
     </li>
   )

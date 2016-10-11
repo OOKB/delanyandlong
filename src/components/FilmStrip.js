6a7ecc4906c6cbe333f30d4167e8d48dcb84622a
Kristian Bjornard <kb@ookb.co>
2016-10-11 12:01:11 -0400

    trying to tweak filmstrip adding new sub components to filmstrip for easier switching of image thumbs

--- src/components/FilmStrip.js
+++ src/components/FilmStrip.js
@@ -1,45 +1,8 @@
 import React, { PropTypes } from 'react'
 import { connectField } from 'redux-field'
 
-import { Link } from 'redux-history-sync'
-import ItemFav from '../containers/ItemFav'
-import ItemImg from './ItemImg'
-import Info from './Info'
-
-function NavItem({ onClick, ...props }) {
-  const imgixExt='?w=500&h=600&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.5&fp-z=2&fp-debug=true'
-  const trimImgixExt = '?w=1500&h=600&fp-debug=true&fit=fill&bg=FFF&fp-x=0.5&fp-y=0.5&fp-z=1&fp-debug=true'
-  return (
-    <li onClick={onClick} role="button" className="button left">
-      <div className="wrap">
-        <ItemImg
-          {...props}
-          imgixExt={imgixExt}
-          />
-      </div>
-    </li>
-  )
-}
-NavItem.propTypes = {
-  onClick: PropTypes.func.isRequired,
-}
-
-function Item({ item }) {
-  const imgixExt = '?w=1500&h=600&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.5&fp-z=1&fp-debug=true'
-  const trimImgixExt = '?w=1500&h=600&fp-debug=true&fit=fill&bg=FFF&fp-x=0.5&fp-y=0.5&fp-z=1&fp-debug=true'
-  return (
-    <li className="left">
-      {/* <Related parent={item} /> */}
-      <Link href={item.link} className="wrap">
-        <ItemImg {...item} imgixExt={imgixExt} />
-      </Link>
-      <Info item={item} />
-    </li>
-  )
-}
-Item.propTypes = {
-  item: PropTypes.object.isRequired,
-}
+import FilmStripItem from './FilmStripItem'
+import FilmStripNavItem from './FilmStripNavItem'
 
 function FilmStrip({ active, formEvent: { onChange }, next, previous }) {
   function clickPrev() { onChange(previous.index) }
@@ -47,9 +10,9 @@ function FilmStrip({ active, formEvent: { onChange }, next, previous }) {
   return (
     <div className="items">
       <ul className="list-reset clearfix filmStrip">
-        <NavItem {...previous.item} onClick={clickPrev} />
-        <Item item={active.item} />
-        <NavItem {...next.item} onClick={clickNext} />
+        <FilmStripNavItem {...previous.item} onClick={clickPrev} />
+        <FilmStripItem item={active.item} />
+        <FilmStripNavItem {...next.item} onClick={clickNext} />
       </ul>
       {!active && <p>No Items</p>}
     </div>
@@ -61,8 +24,6 @@ FilmStrip.propTypes = {
   formEvent: PropTypes.object.isRequired,
   next: PropTypes.object.isRequired,
   previous: PropTypes.object.isRequired,
-  missingImage: PropTypes.func,
-  imgixExt: PropTypes.string.isRequired,
 }
 
 export default connectField()(FilmStrip)
---
+++ src/components/FilmStripItem.js
@@ -0,0 +1,28 @@
+import React, { PropTypes } from 'react'
+import { connectField } from 'redux-field'
+
+import { Link } from 'redux-history-sync'
+import ItemImg from './ItemImg'
+import Info from './Info'
+
+function getImgExt(categoryCode) {
+  if (categoryCode === 'P') return '?w=1500&h=600&fit=fill&bg=FFF'
+  return '?w=1500&h=600&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.5&fp-z=1'
+}
+
+function FilmStripItem({ categoryCode, item }) {
+  return (
+    <li className="left">
+      <Link href={item.link} className="wrap">
+        <ItemImg {...item} imgixExt={getImgExt(categoryCode)} />
+      </Link>
+      <Info item={item} />
+    </li>
+  )
+}
+FilmStripItem.propTypes = {
+  item: PropTypes.object.isRequired,
+  categoryCode: PropTypes.string.isRequired,
+}
+
+export default connectField()(FilmStripItem)

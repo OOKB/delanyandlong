aeb1afc4f1a7486509ec861bb5a90f3d38d60067
Kai Curry <kai@cape.io>
2016-10-11 12:09:56 -0400

    createCollectionList

--- src/redux/select/collection.js
+++ src/redux/select/collection.js
@@ -1,6 +1,6 @@
 import { buildCollectionList, favTitle, getTitle } from 'cape-redux-collection'
 import { selectUser } from 'cape-redux-auth'
-
+import { structuredSelector } from 'cape-select'
 import { getDataFeed, getWebApp } from './'
 
 // Return user if there was a title set. Otherwise return webApp.
@@ -8,5 +8,9 @@ export function collectionListAgent(state, props) {
   if (getTitle(state, props) !== favTitle) return selectUser(state)
   return getWebApp(state)
 }
-export const createCollectionList = buildCollectionList(collectionListAgent, getDataFeed)
+export const listAgentMain = structuredSelector({
+  agent: collectionListAgent,
+  mainEntity: getDataFeed,
+})
+export const createCollectionList = buildCollectionList(listAgentMain)
 export const collectionSelector = {}

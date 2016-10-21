1c9a49ba01d47f60be186782ce48c73e7f8aa065
Kai Curry <kai@cape.io>
2016-10-20 21:01:30 -0500

    prop name is component #108

--- src/components/Detail/DetailFooter.js
+++ src/components/Detail/DetailFooter.js
@@ -1,11 +1,11 @@
 import React, { PropTypes } from 'react'
 import Link from 'redux-history-component'
 
-function DetailFooter({ link: { href, title }, message }) {
+function DetailFooter({ content, link: { href, title } }) {
   return (
     <footer className="detailFooter bg-white small mono z3 text-center">
       <p className="p05 m0">
-        {message}
+        {content}
         <Link href={href} title={title}>{title}</Link>
         .
       </p>
@@ -14,8 +14,8 @@ function DetailFooter({ link: { href, title }, message }) {
 }
 
 DetailFooter.propTypes = {
+  content: PropTypes.string.isRequired,
   link: PropTypes.object.isRequired,
-  message: PropTypes.string.isRequired,
 }
 DetailFooter.defaultProps = {
 }

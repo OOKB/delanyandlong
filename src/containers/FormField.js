f4d0440fc70474b4a87714173360572d975ab4c0
Kai Curry <kai@cape.io>
2016-10-31 21:12:21 -0500

    slight cleanup. could move to redux-field.

--- src/containers/FormField.js
+++ src/containers/FormField.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux'
+import { flow, property } from 'lodash'
 import { getFormEvents, getState } from 'redux-field'
 import { mapDispatchToProps } from 'cape-redux'
 
 import Component from '../components/Fields/Input'
 
-function getActions({ prefix }) {
-  return getFormEvents(prefix)
-}
-export default connect(getState, mapDispatchToProps(getActions))(Component)
+// Used for login.
+
+const formEventActions = mapDispatchToProps(flow(property('prefix'), getFormEvents))
+
+export default connect(getState, formEventActions)(Component)

4037b3fa099ba46298d2680295fc5357324c5db1
Kai Curry <kai@cape.io>
2016-10-31 21:16:07 -0500

    Add Field to add collection

--- src/components/Fav/OverviewEl.js
+++ src/components/Fav/OverviewEl.js
@@ -2,6 +2,17 @@ import React, { PropTypes } from 'react'
 import { map } from 'lodash'
 import Box from './Box'
 import CollectionEl from './CollectionEl'
+import Field from '../../containers/FormField'
+
+const collectionField = {
+  // className: 'collection-title',
+  emptyText: 'Create new collection.',
+  icon: { className: 'light-gray', symbol: 'pencil' },
+  id: 'new-collection',
+  placeholder: 'Title',
+  prefix: [ 'collection', 'title' ],
+  // validate
+}
 
 function Overview({ message, onClose, userCollections }) {
   return (
@@ -9,6 +20,7 @@ function Overview({ message, onClose, userCollections }) {
       <h3 className="m0 fs1 uppercase mono bb1 fw400 mb05">User Collections</h3>
       <ul className="list-reset collections fa-ul">
         {map(userCollections, CollectionEl)}
+        <li><Field {...collectionField} emptyText="new collection" /></li>
       </ul>
     </Box>
   )

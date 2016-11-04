90f62342a081ff1029bfc46dca32f05ff3753a50
Kai Curry <kai@cape.io>
2016-11-03 22:15:52 -0500

    cleanup routes

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import createRouter from 'location-info'
 // import { parse } from 'query-string'
 // const { addRoute, locationInfo } = createRouter({ parseSearch: parse, trailingSlash: false })
 
-const { addRoute, locationInfo } = createRouter()
-addRoute('home', '/')
-addRoute('about', '/about')
-addRoute('contact', '/contact')
-addRoute('pricelist', '/collection(/*)')
-addRoute('detail', '/detail/*')
-addRoute('itemEdit', '/edit/*')
-addRoute('favs', '/favs')
-addRoute('login', '/login')
-addRoute('showroom', '/showroom')
+const { addRoutes, locationInfo } = createRouter()
+addRoutes({
+  home: '/',
+  detail: '/detail/*',
+  itemEdit: '/edit/*',
+  pricelist: '/collection(/*)',
+  project: '/project(/:id)',
+})
+addRoutes([ 'about', 'contact', 'favs', 'login', 'showroom' ])
+
 export default locationInfo

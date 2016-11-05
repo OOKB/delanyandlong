e9bed83245862b26749d2ff8f8c2b0365b248e50
Kristian Bjornard <kb@ookb.co>
2016-11-04 22:32:14 -0400

    nearly have #114 taken care of

--- src/components/Fav/CollectionEl.js
+++ src/components/Fav/CollectionEl.js
@@ -9,7 +9,7 @@ function CollectionEl({ id, inList, onClick, title }) {
       {!inList && <Icon symbol="circle-o" className="fa-li light-gray pointer" />}
       {inList && <Icon symbol="dot-circle-o" className="fa-li green pointer" />}
       <button className="plain pointer relative gold text-left" onClick={onClick}>{title}</button>
-      <Link href={`/project/${id}`}>view</Link>
+      <Link href={`/project/${id}`} className="absolute right-0 view"><Icon symbol="documents" /><span className="hidden">view</span></Link>
     </li>
   )
 }

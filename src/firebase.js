7c68047b684040a1deeebf8b858ac84e204d8f5b
Kai Curry <kai@cape.io>
2016-11-10 16:39:33 -0600

    update reducer

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from 'firebase'
+
+export const config = {
+  apiKey: 'AIzaSyDmoWCc7mwN02lTBrp0mKmh8oeu_k86C5U',
+  authDomain: 'delanylong.firebaseapp.com',
+  databaseURL: 'https://delanylong.firebaseio.com',
+  storageBucket: '',
+  messagingSenderId: '1021052903206',
+}

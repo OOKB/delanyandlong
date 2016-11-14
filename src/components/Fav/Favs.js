c558097a9fffb6a9ca6663a12a5e07a54bc94a24
Kristian Bjornard <kb@ookb.co>
2016-11-13 21:20:42 -0500

    trying to do something about #119, but I have no real clue

--- src/components/Fav/Favs.js
+++ src/components/Fav/Favs.js
@@ -26,9 +26,9 @@ function Favs({ endFavorite, favorites }) {
   return (
     <Page id="favorites">
       <main className="clear m1 mt4 clearfix">
-        <h1 className="text-center m0 bb2 mb1 fw400 uppercase fs1 ls0p15">Your Favorites</h1>
+        {hasFavorites && <h1 className="text-center m0 bb2 mb1 fw400 uppercase fs1 ls0p15">Your Favorites</h1> }
         {hasFavorites && <FavsList endFavorite={endFavorite} favorites={favorites} />}
-        {!hasFavorites && <p className="text-center">You currently have no favorites selected...</p>}
+        {!hasFavorites && <p className="text-center">You currently have no favorites selected...</p> }
       </main>
     </Page>
   )

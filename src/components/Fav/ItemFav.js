91ffdc4b61c07bf6276b2fb0a13d7c41f4ed0e86
Kai Curry <kai@cape.io>
2016-11-17 09:53:13 -0600

    package updates
    running into all kinds of crazy issues

--- src/components/Fav/ItemFav.js
+++ src/components/Fav/ItemFav.js
@@ -1,14 +1,16 @@
 import { partial } from 'lodash'
 import { connect } from 'react-redux'
 import { mapDispatchToProps } from 'cape-redux'
-import { mapStateToProps, toggle } from 'cape-redux-collection'
+import * as collection from 'cape-redux-collection'
 
 import Component from './ItemFavEl'
+
+console.log('collection', collection)
 
 function getActions({ item }) {
   return {
     editItemCollections: partial(toggle, item),
   }
 }
-const actions = mapDispatchToProps(getActions)
+const actions = {} // mapDispatchToProps(getActions)
 export default connect(mapStateToProps, actions)(Component)

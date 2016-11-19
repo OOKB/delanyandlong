5760ccc4367c7ec28d91f5314c78d37213a3a218
Kai Curry <kai@cape.io>
2016-11-19 15:58:09 -0600

    dynamic favs link to specific project

--- src/redux/select/menu.js
+++ src/redux/select/menu.js
@@ -1,13 +1,27 @@
-import { createStructuredSelector } from 'reselect'
+import { createSelector, createStructuredSelector } from 'reselect'
 import { logout } from 'cape-redux-auth'
-
+import { keyBy } from 'lodash/fp'
+import { setIn } from 'cape-redux'
+import { favsListSelector } from 'cape-redux-collection'
 import { getDb } from './'
 import { filterPerms } from './perms'
 import { getRouteId } from '../routing'
 
 export const getMenu = getDb('menu')
 
-export const menuItems = filterPerms(getMenu)
+export function projectLink({ id }) {
+  return `/project/${id}`
+}
+
+export const menuItems = createSelector(
+  filterPerms(getMenu),
+  favsListSelector,
+  (items, favsList) => {
+    const menu = keyBy('id')(items)
+    if (menu.favs && favsList) return setIn([ 'favs', 'href' ], menu, projectLink(favsList))
+    return menu
+  }
+)
 
 // Used for the component state.
 export const menuSelector = createStructuredSelector({

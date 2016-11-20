af0dc775c11bff930fa0569bc615ddf96bf09bdf
Kai Curry <kai@cape.io>
2016-11-20 11:09:23 -0600

    Make storeListener more functional

--- src/redux/fire/storeListener.js
+++ src/redux/fire/storeListener.js
@@ -1,8 +1,8 @@
-import { flow, isEmpty, partial, partialRight, values } from 'lodash'
+import { curry, flow, isEmpty, partial, partialRight, values } from 'lodash'
 import { addListener } from 'cape-redux'
 import { entityPut, selectEntityById } from 'redux-graph'
 import { isAnonymous, login, loginRedirect, logout, selectToken, setUserId } from 'cape-redux-auth'
-import { ENTITY_PUT, ENTITY_PUTALL } from '@kaicurry/redux-graph'
+import { ENTITY_PUT, ENTITY_PUTALL, getEntity } from '@kaicurry/redux-graph'
 import { COLLECTION_TYPE, LIST_ITEM } from 'cape-redux-collection'
 
 export function resAct(dispatch, action) {
@@ -39,34 +39,37 @@ function handleAuth({ dispatch, getState }, { auth, user }, usr) {
 function handleLogout(store, isAnon, { auth }) {
   if (isAnon) auth.signOut()
 }
+export const handleChanged = curry(({ dispatch, getState }, change) => {
+  const newVal = change.val()
+  const oldVal = getEntity(getState(), newVal)
+  if (oldVal || newVal.dateModified === oldVal.dateModified) return null
+  return dispatch({ type: ENTITY_PUT, payload: newVal })
+})
+export const typeListener = curry((store, { entity }, typeId) =>
+  entity.child(typeId).orderByChild('dateModified').limitToLast(1)
+  .on('child_changed', handleChanged(store))
+)
+export const handleInit = curry(({ dispatch }, result) => {
+  const payload = values(result.val())
+  if (isEmpty(payload)) return null
+  return dispatch({ type: ENTITY_PUTALL, payload })
+})
+export const typeLoader = curry((store, { entity }, typeId) =>
+  entity.child(typeId).once('value')
+  .then(handleInit(store))
+)
 export default function storeListener(store, firebase) {
   addListener(selectToken, store, partialRight(handleLoginToken, firebase))
   firebase.auth.onAuthStateChanged(partial(handleAuth, store, firebase))
   addListener(isAnonymous, store, partialRight(handleLogout, firebase))
-  firebase.entity.child('OrderTrackItem').once('value')
-  .then(items =>
-    store.dispatch({ type: ENTITY_PUTALL, payload: values(items.val()) })
-  )
-  .then(() => firebase.entity.child(LIST_ITEM).once('value'))
-  .then(listItems =>
-    store.dispatch({ type: ENTITY_PUTALL, payload: values(listItems.val()) })
-  )
-  // Add db lists to redux.
-  .then(() => firebase.entity.child(COLLECTION_TYPE).once('value'))
-  .then((lists) => {
-    const payload = values(lists.val())
-    if (isEmpty(payload)) return null
-    // console.log(payload)
-    return store.dispatch({ type: ENTITY_PUTALL, payload })
-  })
-  firebase.entity.child(LIST_ITEM).orderByChild('dateModified').limitToLast(1)
-  .on('child_changed', (node) => {
-    store.dispatch({ type: ENTITY_PUT, payload: node.val() })
-  })
-  // Listen for changes to Lists.
-  firebase.entity.child(COLLECTION_TYPE).orderByChild('dateModified').limitToLast(1)
-  .on('child_changed', (node) => {
-    store.dispatch({ type: ENTITY_PUT, payload: node.val() })
-  })
+  // FIREBASE LOAD to REDUX
+  const addTypeLoader = typeLoader(store, firebase)
+  addTypeLoader('OrderTrackItem')
+  .then(() => addTypeLoader(LIST_ITEM))
+  .then(() => addTypeLoader(COLLECTION_TYPE))
+  // FIREBASE LISTENERS to REDUX
+  const addTypeListener = typeListener(store, firebase)
+  addTypeListener(LIST_ITEM)
+  addTypeListener(COLLECTION_TYPE)
   return store
 }

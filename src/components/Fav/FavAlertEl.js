b2f77eb6b05776dabbd8ad8192de4eb360aa9b29
Kai Curry <kai@cape.io>
2016-11-20 21:34:03 -0600

    hacks to get projectLink working

--- src/components/Fav/FavAlertEl.js
+++ src/components/Fav/FavAlertEl.js
@@ -3,27 +3,34 @@ import Link from 'redux-history-component'
 
 import Box from './Box'
 import Field from '../Editable/FieldWrapper'
+import { projectLink } from '../../redux/collection'
 
-function FavAlert({ item, listItem, message, onClose, schema }) {
-  const collectionUrl = `/project/${listItem.id}`
+function FavAlert({ listItem, message, onClose, schema, viewText }) {
+  const collectionUrl = projectLink(listItem.list)
   return (
     <Box onClose={onClose} message={message}>
       <ul className="list-reset mb1">
         <li><Field {...schema.position} /></li>
         <li><Field {...schema.description} /></li>
       </ul>
-      <Link className="small uppercase" href={collectionUrl} onClick={onClose}>View and share</Link>
+      {collectionUrl &&
+        <Link className="small uppercase" href={collectionUrl} onClick={onClose}>
+          {viewText}
+        </Link>
+      }
       <div className="small" style={{ color: 'burlywood' }}>ListItem ID: {listItem.id}</div>
     </Box>
   )
 }
 
 FavAlert.propTypes = {
-  item: PropTypes.object.isRequired,
   listItem: PropTypes.object.isRequired,
   message: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
   schema: PropTypes.object,
+  viewText: PropTypes.string.isRequired,
 }
-
+FavAlert.defaultProps = {
+  viewText: 'View and share',
+}
 export default FavAlert

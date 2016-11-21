fab2689e86c3c67f4f57d59ce6a848cefafeca92
Kai Curry <kai@cape.io>
2016-11-21 10:03:45 -0600

    split NavItem

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,33 +1,17 @@
 import React, { PropTypes } from 'react'
 import map from 'lodash/map'
-import classnames from 'classnames'
-import Link from 'redux-history-component'
 import { connect } from 'react-redux'
-
 import { menuActions, menuSelector } from '../redux/select/menu'
-import Icon from './Icon'
-
-function NavItem({ active, action, id, href, label, icon }) {
-  const Label = (<span>
-    {icon && <Icon className="visible-sm" symbol={icon} hidden />}
-    <span className="visible-lg">{label}</span>
-  </span>)
-
-  return (
-    <li className={classnames(id, { active })}>
-      {href && <Link href={href} alt={label} title={label}>{Label}</Link>}
-      {action && <button onClick={action}>{Label}</button>}
-    </li>
-  )
-}
+import NavItem from './NavItem'
 
 function Menu({ activeId, links, logout }) {
   function isActive({ id }) { return activeId === id }
-  function getAction({ action }) { return action === 'logout' && logout }
+  function getAction({ action }) { return (action === 'logout' && logout) || undefined }
+  console.log('menu')
   return (
     <ul className="menu list-reset list-inline">
-      {map(links, props => (
-        <NavItem {...props} key={props.id} active={isActive(props)} action={getAction(props)} />
+      {map(links, link => (
+        <NavItem {...link} key={link.id} active={isActive(link)} action={getAction(link)} />
       ))}
     </ul>
   )

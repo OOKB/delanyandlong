4f40290dea034517e4e6f1a89aeb8188bacd8557
Kristian Bjornard <kb@ookb.co>
2016-11-20 22:06:23 -0500

    minor improvement

--- src/components/Fav/ProjectsEl.js
+++ src/components/Fav/ProjectsEl.js
@@ -11,7 +11,7 @@ function ProjectsEl({ lists }) {
         <h1 className="text-center m0 mb1 uppercase fs1 ls0p15">Projects</h1>
         <ul className="list-reset clearfix text-center mt1 bb1 five mlrauto">
           {map(lists, list =>
-            <li className="p1 bt1" key={list.id}><Link className="block uppercase" href={projectLink(list)}>{list.title}</Link></li>
+            <li className="p1 bt1 fs0p8 ls0p15" key={list.id}><Link className="block uppercase" href={projectLink(list)}>{list.title}</Link></li>
           )}
         </ul>
       </main>

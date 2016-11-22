ba5311f6cbfb0bf50d4eae6727d8d360096f587b
Kristian Bjornard <kb@ookb.co>
2016-11-22 11:44:46 -0500

    a bunch of errors in the showrooms file

--- src/redux/showrooms.js
+++ src/redux/showrooms.js
@@ -1,243 +1,225 @@
 export default [
   {
-    "id": "rgheadquarters",
-    "title": "Corporate Headquarters",
-    "address": [
-      "41 Chestnut Street",
-      "Greenwich CT 06830"
+    id: 'rgheadquarters',
+    title: 'Corporate Headquarters',
+    address: [
+      '41 Chestnut Street',
+      'Greenwich CT 06830',
     ],
-    "telephone": "203-532-8068",
+    telephone: '203-532-8068',
     type: 'Showroom',
-    "fax": "203-532-9514"
+    'fax': '203-532-9514',
   },
   {
-    "id": "NYCshowroom",
-    "title": "New York Showroom",
-    "name": "Rogers & Goffigon",
-    "address": [
-      "979 3rd Avenue",
-      "Suite 1718",
-      "New York NY 10022"
+    id: 'NYCshowroom',
+    title: 'New York Showroom',
+    name: 'Rogers & Goffigon',
+    address: [
+      '979 3rd Avenue',
+      'Suite 1718',
+      'New York NY 10022',
     ],
-    "email": "nycshowroom@rogersandgoffigon.com",
+    email: 'nycshowroom@rogersandgoffigon.com',
     type: 'Showroom',
-    "telephone": "212-888-3242"
+    telephone: '212-888-3242',
   },
   {
-    "id": "dlheadquarters",
-    "title": "Corporate Headquarters",
-    "address": [
-      "41 Chestnut Street",
-      "Greenwich CT 06830"
+    id: 'dlheadquarters',
+    title: 'Corporate Headquarters',
+    address: [
+      '41 Chestnut Street',
+      'Greenwich CT 06830',
     ],
-    "email": "customerservice@delanyandlong.com",
-    "telephone": "203.532.0010",
+    email: 'customerservice@delanyandlong.com',
+    telephone: '203.532.0010',
     type: 'Showroom',
-    "fax": "203.532.0032"
+    'fax': '203.532.0032',
   },
   {
-    "id": "ariz",
-    "title": "Arizona Showroom",
-    "name": "TOWN",
-    "address": [
-      "2716 North 68th Street, Suite 7",
-      "Scottsdale, AZ 85257"
+    id: 'bostonNE',
+    title: 'Boston and Northeast US',
+    name: 'Karen Walls',
+    email: 'karen@rogersandgoffigon.com',
+    type: 'Showroom',
+    telephone: '617.480.2135',
+  },
+  {
+    id: 'chicago',
+    title: 'Chicago Showroom',
+    name: 'Cowtan & Tout',
+    address: [
+      '638 Merchandise Mart',
+      'Chicago, IL 60654',
     ],
-    "email": "infoAZ@townstudio.com",
-    "telephone": "480.214.4792",
+    email: 'nancy_cordin@cowtan.com',
     type: 'Showroom',
-    "fax": "602.297.6821"
+    telephone: '312.644.0717',
   },
   {
-    "id": "bostonNE",
-    "title": "Boston and Northeast US",
-    "name": "Karen Walls",
-    "email": "karen@rogersandgoffigon.com",
+    id: 'connecticutEtc',
+    title: 'Connecticut, Rhode Island, Long Island & Westchester Representative',
+    name: 'Sarah Conduit',
+    email: 'sarah@rogersandgoffigon.com',
     type: 'Showroom',
-    "telephone": "617.480.2135"
+    telephone: '203.247.6514',
   },
   {
-    "id": "chicago",
-    "title": "Chicago Showroom",
-    "name": "Cowtan & Tout",
-    "address": [
-      "638 Merchandise Mart",
-      "Chicago, IL 60654"
+    id: 'dallas',
+    title: 'Dallas Showroom',
+    name: 'George Cameron Nash',
+    address: [
+      '1025 North Stemmons Freeway',
+      'Suite 150',
+      'Dallas TX 75207',
     ],
-    "email": "nancy_cordin@cowtan.com",
+    email: 'noelm@gcnash.com',
     type: 'Showroom',
-    "telephone": "312.644.0717"
+    telephone: '214.744.1544',
   },
   {
-    "id": "mountainStates",
-    "title": "Colorado and Mountain States Showroom",
-    "name": "TOWN",
-    "address": [
-      "601 South Broadway",
-      "Suite A",
-      "Denver CO 80209"
+    id: 'washingtonDC',
+    title: 'DC Showroom',
+    name: 'Cowtan & Tout',
+    address: [
+      'Washington Design Center',
+      '1099 14th Street NW, suite 310',
+      'Washington, DC 20005',
     ],
-    "email": "jeremy@townstudio.com",
+    email: 'carol_reilly@cowtan.com',
     type: 'Showroom',
-    "telephone": "303.282.8696"
+    telephone: '202.863.8490',
   },
   {
-    "id": "connecticutEtc",
-    "title": "Connecticut, Rhode Island, Long Island & Westchester Representative",
-    "name": "Sarah Conduit",
-    "email": "sarah@rogersandgoffigon.com",
-    type: 'Showroom',
-    "telephone": "203.247.6514"
-  },
-  {
-    "id": "dallas",
-    "title": "Dallas Showroom",
-    "name": "George Cameron Nash",
-    "address": [
-      "1025 North Stemmons Freeway",
-      "Suite 150",
-      "Dallas TX 75207"
+    id: 'florida',
+    title: 'Florida Showroom',
+    name: 'Cowtan & Tout',
+    address: [
+      '1855 Griffin Road',
+      'Suite B 254',
+      'Dania FL 33004',
     ],
-    "email": "noelm@gcnash.com",
+    email: 'noelle_collins@cowtan.com',
     type: 'Showroom',
-    "telephone": "214.744.1544"
+    telephone: '954.920.1453',
   },
   {
-    "id": "washingtonDC",
-    "title": "DC Showroom",
-    "name": "Cowtan & Tout",
-    "address": [
-      "Washington Design Center",
-      "1099 14th Street NW, suite 310",
-      "Washington, DC 20005"
+    id: 'houston',
+    title: 'Houston Showroom',
+    name: 'George Cameron Nash',
+    address: [
+      '5120 Woodway',
+      'Suite 140',
+      'Houston TX 77056',
     ],
-    "email": "carol_reilly@cowtan.com",
+    email: 'cynthiaw@gcnash.com',
     type: 'Showroom',
-    "telephone": "202.863.8490"
+    telephone: '713.892.5710',
   },
   {
-    "id": "florida",
-    "title": "Florida Showroom",
-    "name": "Cowtan & Tout",
-    "address": [
-      "1855 Griffin Road",
-      "Suite B 254",
-      "Dania FL 33004"
+    id: 'midwest',
+    title: 'Iowa, Kansas, Minnesota, Missouri, Nebraska, North Dakota, South Dakota Representative',
+    name: 'blakeConcepts LLC',
+    email: 'leah@blakeConcepts.com',
+    type: 'Showroom',
+    telephone: '312.505.5249',
+  },
+  {
+    id: 'laWestCoast',
+    title: 'Los Angeles Showroom',
+    name: 'Cowtan & Tout',
+    address: [
+      '8687 Melrose Avenue',
+      'Space B 647',
+      'Los Angeles CA 90069',
     ],
-    "email": "noelle_collins@cowtan.com",
+    email: 'gina_marra@cowtan.com',
     type: 'Showroom',
-    "telephone": "954.920.1453"
+    telephone: '310.659.1423',
   },
   {
-    "id": "houston",
-    "title": "Houston Showroom",
-    "name": "George Cameron Nash",
-    "address": [
-      "5120 Woodway",
-      "Suite 140",
-      "Houston TX 77056"
+    id: 'newjersey',
+    title: 'Northern New Jersey Representative',
+    name: 'Blake Haase',
+    email: 'blake@rogersandgoffigon.com',
+    type: 'Showroom',
+    telephone: '845.863.9671',
+  },
+  {
+    id: 'midatlantic',
+    title: 'Pennsylvania, Southern NJ, Delaware Showroom',
+    name: 'Harrington Group',
+    address: [
+      '2400 Market Street',
+      'Suite 309',
+      'Philadelphia PA 19103',
     ],
-    "email": "cynthiaw@gcnash.com",
+    email: 'sharrington@shaenharringtoninc.com',
     type: 'Showroom',
-    "telephone": "713.892.5710"
+    telephone: '215.567.2181',
   },
   {
-    "id": "midwest",
-    "title": "Iowa, Kansas, Minnesota, Missouri, Nebraska, North Dakota, South Dakota Representative",
-    "name": "blakeConcepts LLC",
-    "email": "leah@blakeConcepts.com",
+    id: 'rockies',
+    title: 'Rocky Mountain Representative',
+    name: 'Richard Moreau Lovelace',
+    email: 'richard@rogersandgoffigon.com',
     type: 'Showroom',
-    "telephone": "312.505.5249"
+    telephone: 'Cell: 303.999.6703, Office: 720.458.0278',
   },
   {
-    "id": "laWestCoast",
-    "title": "Los Angeles Showroom",
-    "name": "Cowtan & Tout",
-    "address": [
-      "8687 Melrose Avenue",
-      "Space B 647",
-      "Los Angeles CA 90069"
+    id: 'sanfranWestCoast',
+    title: 'San Francisco Showroom',
+    name: 'Kneedler-Fauchere',
+    address: [
+      '101 Henry Adams Street',
+      'Suite 170',
+      'San Francisco CA 94103',
     ],
-    "email": "gina_marra@cowtan.com",
+    email: 'sanfranciscoinfo@kneedlerfauchere.com',
     type: 'Showroom',
-    "telephone": "310.659.1423"
+    telephone: '415.487.6180',
   },
   {
-    "id": "newjersey",
-    "title": "Northern New Jersey Representative",
-    "name": "Blake Haase",
-    "email": "blake@rogersandgoffigon.com",
+    id: 'southeast',
+    title: 'Southeastern US, Ohio, Kentucky, Indiana Representative',
+    name: 'Luane Goldstone',
+    email: 'luane@rogersandgoffigon.com',
     type: 'Showroom',
-    "telephone": "845.863.9671"
+    telephone: '843.513.2208',
   },
   {
-    "id": "midatlantic",
-    "title": "Pennsylvania, Southern NJ, Delaware Showroom",
-    "name": "Harrington Group",
-    "address": [
-      "2400 Market Street",
-      "Suite 309",
-      "Philadelphia PA 19103"
+    id: 'northwest',
+    title: 'Northwest (US & Canada) Representative',
+    name: 'Peg Winship',
+    email: 'peg@rogersandgoffigon.com',
+    type: 'Showroom',
+    telephone: '206.618.4145',
+  },
+  {
+    id: 'pacificrim',
+    title: 'Australia, New Zealand, Singapore Representative',
+    name: 'Caterina Ferrari',
+    email: 'caterina@rogersandgoffigon.com',
+    type: 'Showroom',
+    telephone: '+61 (0)4 2213 0584',
+  },
+  {
+    id: 'europe',
+    title: 'European Representative',
+    name: 'Please call our main customer service number',
+    type: 'Showroom',
+    telephone: '001.203.532.0010',
+  },
+  {
+    id: 'canada',
+    title: 'Toronto Showroom',
+    name: 'Primavera Interior Furnishings, Ltd.',
+    address: [
+      '160 Pears Avenue',
+      'Suite 110',
+      'Toronto ON M5R 3P8 Canada',
     ],
-    "email": "sharrington@shaenharringtoninc.com",
+    email: 'diego@primavera.ca',
     type: 'Showroom',
-    "telephone": "215.567.2181"
+    telephone: '416.921.3334',
   },
-  {
-    "id": "sanfranWestCoast",
-    "title": "San Francisco Showroom",
-    "name": "Kneedler-Fauchere",
-    "address": [
-      "101 Henry Adams Street",
-      "Suite 170",
-      "San Francisco CA 94103"
-    ],
-    "email": "sanfranciscoinfo@kneedlerfauchere.com",
-    type: 'Showroom',
-    "telephone": "415.487.6180"
-  },
-  {
-    "id": "southeast",
-    "title": "Southeastern US, Ohio, Kentucky, Indiana Representative",
-    "name": "Luane Goldstone",
-    "email": "luane@rogersandgoffigon.com",
-    type: 'Showroom',
-    "telephone": "843.513.2208"
-  },
-  {
-    "id": "northwest",
-    "title": "Northwest (US & Canada) Representative",
-    "name": "Peg Winship",
-    "email": "peg@rogersandgoffigon.com",
-    type: 'Showroom',
-    "telephone": "206.618.4145"
-  },
-  {
-    "id": "pacificrim",
-    "title": "Australia, New Zealand, Singapore Representative",
-    "name": "Caterina Ferrari",
-    "email": "caterina@rogersandgoffigon.com",
-    type: 'Showroom',
-    "telephone": "+61 (0)4 2213 0584"
-  },
-  {
-    "id": "europe",
-    "title": "European Representative",
-    "name": "Please call our main customer service number",
-    type: 'Showroom',
-    "telephone": "001.203.532.0010"
-  },
-  {
-    "id": "canada",
-    "title": "Toronto Showroom",
-    "name": "Primavera Interior Furnishings, Ltd.",
-    "address": [
-      "160 Pears Avenue",
-      "Suite 110",
-      "Toronto ON M5R 3P8 Canada"
-    ],
-    "email": "diego@primavera.ca",
-    type: 'Showroom',
-    "telephone": "416.921.3334"
-  }
 ]

e6a8187e836c6bccf2d9a5dcafb411f386245aa4
Kai Curry <kai@cape.io>
2016-11-23 11:10:27 -0600

    Drawer container

--- src/components/DrawerEl.js
+++ src/components/DrawerEl.js
@@ -1,12 +1,13 @@
 import React, { PropTypes } from 'react'
+import { partial } from 'lodash'
 import Link from 'redux-history-component'
 import Close from './CloseButton'
 
-function Drawer({ blurb, collectionLink, img, onClose, sisterSite, title }) {
+function Drawer({ blurb, collectionLink, id, img, onClose, sisterSite, title, user }) {
   return (
     <section className="drawer bg-light-gold p2 relative inset-shadow">
 
-      <Close onClick={onClose} className="close right-1 top-1" />
+      <Close onClick={partial(onClose, user, id)} className="close right-1 top-1" />
 
       <div className="mb0 pb0 maxw50rem mlrauto clearfix">
 
@@ -46,21 +47,22 @@ Drawer.propTypes = {
   onClose: PropTypes.func.isRequired,
   sisterSite: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
+  user: PropTypes.object.isRequired,
 }
 Drawer.defaultProps = {
   blurb: 'Among many features, the DeLany & Long website links to the Rogers & Goffigon website, so you can easily view both collections.',
-  disclaimer: 'All fabrics sold as is. All sales Final. No returns or exchanges. Payment by check or credit card.',
-  id: 'newSite',
-  img: 'https://delanyandlong.imgix.net/drawer/20161123-dlAnouncement.jpg?w=400',
-  title: 'DeLany & Long is excited to announce a brand new website!',
   collectionLink: {
     title: 'Explore the Delany & Long collection.',
     href: 'collection',
   },
+  disclaimer: 'All fabrics sold as is. All sales Final. No returns or exchanges. Payment by check or credit card.',
+  id: 'newSite',
+  img: 'https://delanyandlong.imgix.net/drawer/20161123-dlAnouncement.jpg?w=400',
   sisterSite: {
     title: 'Explore the Rogers & Goffigon collection.',
     href: 'http://www.rogersandgoffigon.com/',
   },
+  title: 'DeLany & Long is excited to announce a brand new website!',
 }
 
 export default Drawer

ebe264254f6aff9b3af33e6b3ffa2f7a7c7303a6
Kai Curry <kai@cape.io>
2016-11-23 10:10:30 -0600

    eslint

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -2,29 +2,29 @@ import React, { PropTypes } from 'react'
 import Link from 'redux-history-component'
 import Close from './CloseButton'
 
-function Drawer({ blurb, collectionLink, disclaimer, onClose, sisterSite, title }) {
+function Drawer({ blurb, collectionLink, disclaimer, img, onClose, sisterSite, title }) {
   return (
     <section className="drawer bg-light-gold p2 relative inset-shadow">
-
       <Close onClick={onClose} className="right-1 top-1" />
-
       <div className="mb0 pb0 maxw50rem mlrauto clearfix">
-
-        <img alt="a thing" className="six columns mb0 b1 p05 white" src="https://b2.cape.io/file/delanyandlong/test7.jpg" />
-
+        <img
+          alt="a thing"
+          className="six columns mb0 b1 p05 white"
+          src={img}
+        />
         <div className="six columns mb0">
           <h1 className="dark-gold m0 mb05">{title}</h1>
           <p className="m0">{blurb}</p>
           <p className="mt1 p0 mono">
-            <Link href={collectionLink.href} title={collectionLink.title}>{collectionLink.title}</Link>
+            <Link href={collectionLink.href} title={collectionLink.title}>
+              {collectionLink.title}
+            </Link>
           </p>
           <p className="mt1 p0 mono">
             <Link href={sisterSite.href} title={sisterSite.title}>{sisterSite.title}</Link>
           </p>
         </div>
-
       </div>
-
     </section>
   )
 }
@@ -33,6 +33,8 @@ Drawer.propTypes = {
   blurb: PropTypes.string.isRequired,
   collectionLink: PropTypes.object.isRequired,
   disclaimer: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
   sisterSite: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
@@ -40,6 +42,8 @@ Drawer.propTypes = {
 Drawer.defaultProps = {
   blurb: 'Among many features, the DeLany & Long website links to the Rogers & Goffigon website, so you can easily view both collections.',
   disclaimer: 'All fabrics sold as is. All sales Final. No returns or exchanges. Payment by check or credit card.',
+  id: 'newSite',
+  img: 'https://b2.cape.io/file/delanyandlong/test7.jpg',
   title: 'DeLany & Long is excited to announce a brand new website!',
   collectionLink: {
     title: 'Explore the Delany & Long collection here.',

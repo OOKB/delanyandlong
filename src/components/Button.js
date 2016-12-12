319ffc10a416f67340ccfd53d323d5a18274cfa4
Kai Curry <kai@cape.io>
2016-12-12 10:17:15 -0600

    button does not require icon

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,20 @@
 import React, { PropTypes } from 'react'
 import classnames from 'classnames'
 
-function Button({ className, icon, ...props }) {
+function Button({ children, className, icon, ...props }) {
   return (
     <button {...props} className={classnames('absolute btn-block', className)}>
-      <i className={classnames('fa', `fa-${icon}`)} aria-hidden="true" />
+      {icon && <i className={classnames('fa', `fa-${icon}`)} aria-hidden="true" />}
+      {children}
     </button>
   )
 }
 
 Button.propTypes = {
+  children: PropTypes.node,
   className: PropTypes.string,
   onClick: PropTypes.func.isRequired,
-  icon: PropTypes.string.isRequired,
+  icon: PropTypes.string,
 }
 
 export default Button

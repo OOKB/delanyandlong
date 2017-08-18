e01322ad571684d026017aa28aa9f6ca68eaba03
Kai Curry <kai@cape.io>
2017-08-18 15:44:41 -0400

    use function calls instead of JSX

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import { createElement } from 'react'
 import { render } from 'react-dom'
-// Add the latest es2016+ stuff that we love.
-// import 'babel-polyfill'
 
 import configureStore from './redux/configureStore'
 // Root React component.
 import Root from './redux/Root'
+
+/* global window */
 
 // Define our inital state object. This could be a fetch() to an API endpoint.
 const initialState = window.reactData || {}
@@ -13,7 +13,7 @@ const initialState = window.reactData || {}
 const store = configureStore(initialState)
 
 // Define our destination where we insert our root react component.
-const destEl = document.getElementById('root')
+const destEl = window.document.getElementById('root')
 
 // The root component needs the Redux `store`.
-render(<Root store={store} />, destEl)
+render(createElement(Root, { store }), destEl)

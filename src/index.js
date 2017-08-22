cfbe18e8d0c7e039541f4d799ebf979df0d66062
Kai Curry <kai@cape.io>
2017-08-22 16:59:43 -0400

    Try using cape-router-component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import { createElement } from 'react'
-import { render } from 'react-dom'
+import { renderRoot } from 'cape-router-component'
 
 import configureStore from './redux/configureStore'
-// Root React component.
-import Root from './redux/Root'
+import * as RouteIndex from './RouteIndex'
 
 /* global window */
 
@@ -13,7 +12,7 @@ const initialState = window.reactData || {}
 const store = configureStore(initialState)
 
 // Define our destination where we insert our root react component.
-const destEl = window.document.getElementById('root')
+const rootDestEl = window.document.getElementById('root')
 
 // The root component needs the Redux `store`.
-render(createElement(Root, { store }), destEl)
+renderRoot({ rootDestEl, RouteIndex, store })
